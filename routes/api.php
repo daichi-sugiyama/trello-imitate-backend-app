783317daefd55d4f14ce02c54fc4afcc7f09048c
daichi.sugiyama <daichi.sugiyama@northdetail.co.jp>
2020-11-06 11:09:16 +0900

    APIのget通信でjsonをレスポンスするように実装

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['api']], function(){
+    Route::resource('list', 'Api\ListController', ['except' => ['show', 'update', 'destroy' ,'create', 'edit']]);
+});
