004078487c9a7663b3cb3a0c5e777f1a776e0d8e
daichi.sugiyama <d.sugiyama421@gmail.com>
2020-11-08 13:53:50 +0900

    jsonを更新できるように実装

--- app/Http/Controllers/Api/ListController.php
+++ app/Http/Controllers/Api/ListController.php
@@ -9,16 +9,17 @@ use App\Lists;
 
 class ListController extends Controller {
 	// TODO:DBに保存したい
-	private $json= '[{"listId": 1,"listTitle": "リスト1","cardData": [{"cardId": 11,"cardTitle": "カードタイトル11"},{"cardId": 12,"cardTitle": "カードタイトル12"},{"cardId": 13,"cardTitle": "カードタイトル13"}]},{"listId": 2,"listTitle": "リスト2","cardData": [{"cardId": 21,"cardTitle": "カードタイトル21"},{"cardId": 22,"cardTitle": "カードタイトル22"},{"cardId": 23,"cardTitle": "カードタイトル23"}]},{"listId": 3,"listTitle": "リスト3","cardData": [{"cardId": 3,"cardTitle": "カードタイトル31"},{"cardId": 32,"cardTitle": "カードタイトル32"},{"cardId": 33,"cardTitle": "カードタイトル33"}]},{"listId": 4,"listTitle": "リスト4","cardData": [{"cardId": 4,"cardTitle": "カードタイトル41"},{"cardId": 42,"cardTitle": "カードタイトル42"},{"cardId": 43,"cardTitle": "カードタイトル43"}]},{"listId": 5,"listTitle": "リスト5","cardData": [{"cardId": 51,"cardTitle": "カードタイトル51"},{"cardId": 52,"cardTitle": "カードタイトル52"},{"cardId": 53,"cardTitle": "カードタイトル53"}]}]';
 
 	public function index() {
+		// DBから取得
 		$list = Lists::find(1);
 		return response()->json(json_decode($list->json));
 	}
 
 	public function store(Request $request) {
-		// TODO:DBに登録
-		dd($request);
-		return response()->json($request);
+		// DBに登録
+		$json = json_encode($request->input());
+		$list = Lists::find(1);
+		$list->update(['json' => $json]);
 	}
 }
--- app/Lists.php
+++ app/Lists.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Lists extends Model
 {
     protected $primaryKey = 'list_id';
+    protected $guarded = ['list_id'];
 }
